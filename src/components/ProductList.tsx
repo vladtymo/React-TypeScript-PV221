86c19e4dfab6b08f5cdc8029ea2d38815f6ce2df
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-05-15 18:40:52 +0300

    Implement delete logic

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,80 +1,81 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Popconfirm, Space, Table, Tag } from 'antd';
+import { Button, Popconfirm, Space, Table, Tag, message } from 'antd';
 import type { TableProps } from 'antd';
 import { ProductModel } from '../models/product.model';
 import { productsService } from '../services/products.service';
 import { Link } from 'react-router-dom';
 import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
 
-const columns: TableProps<ProductModel>['columns'] = [
-    {
-        title: 'Id',
-        dataIndex: 'id',
-        key: 'id',
-        sorter: {
-            compare: (a, b) => a.id - b.id, // less <0 equsls 0> bigger
+const getColumns = (deleteHandler: (id: number) => void): TableProps<ProductModel>['columns'] =>
+    [
+        {
+            title: 'Id',
+            dataIndex: 'id',
+            key: 'id',
+            sorter: {
+                compare: (a, b) => a.id - b.id, // less <0 equsls 0> bigger
+            },
         },
-    },
-    {
-        title: 'Image',
-        dataIndex: 'imageUrl',
-        key: 'imageUrl',
-        render: (text, record) => <img style={imageStyles} src={text} alt={record.name} />
-    },
-    {
-        title: 'Name',
-        dataIndex: 'name',
-        key: 'name',
-        render: (text) => <a href='/'>{text}</a>,
-        sorter: {
-            compare: (a, b) => a.name.localeCompare(b.name)
+        {
+            title: 'Image',
+            dataIndex: 'imageUrl',
+            key: 'imageUrl',
+            render: (text, record) => <img style={imageStyles} src={text} alt={record.name} />
         },
-    },
-    {
-        title: 'Price',
-        dataIndex: 'price',
-        key: 'price',
-        render: (text) => <span>{text}$</span>,
-        sorter: {
-            compare: (a, b) => a.price - b.price
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+            render: (text) => <a href='/'>{text}</a>,
+            sorter: {
+                compare: (a, b) => a.name.localeCompare(b.name)
+            },
         },
-    },
-    {
-        title: 'Category',
-        dataIndex: 'categoryName',
-        key: 'categoryName'
-    },
-    {
-        title: 'Discount',
-        dataIndex: 'discount',
-        key: 'discount',
-        render: (text) => <span>{text}%</span>,
-        sorter: {
-            compare: (a, b) => a.discount - b.discount
+        {
+            title: 'Price',
+            dataIndex: 'price',
+            key: 'price',
+            render: (text) => <span>{text}$</span>,
+            sorter: {
+                compare: (a, b) => a.price - b.price
+            },
         },
-    },
-    {
-        title: 'Action',
-        key: 'action',
-        render: (_, record) => (
-            <Space size="middle">
-                <Link to={`edit/${record.id}`}>
-                    <Button icon={<EditOutlined />}></Button>
-                </Link>
-                <Popconfirm
-                    title="Delete the product"
-                    description={`Are you sure to delete ${record.name}?`}
-                    // onConfirm={() => deleteHandler(record.id)}
-                    okText="Yes"
-                    cancelText="No"
-                    placement="left"
-                >
-                    <Button danger icon={<DeleteOutlined />}></Button>
-                </Popconfirm>
-            </Space>
-        ),
-    },
-];
+        {
+            title: 'Category',
+            dataIndex: 'categoryName',
+            key: 'categoryName'
+        },
+        {
+            title: 'Discount',
+            dataIndex: 'discount',
+            key: 'discount',
+            render: (text) => <span>{text}%</span>,
+            sorter: {
+                compare: (a, b) => a.discount - b.discount
+            },
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            render: (_, record) => (
+                <Space size="middle">
+                    <Link to={`edit/${record.id}`}>
+                        <Button icon={<EditOutlined />}></Button>
+                    </Link>
+                    <Popconfirm
+                        title="Delete the product"
+                        description={`Are you sure to delete ${record.name}?`}
+                        onConfirm={() => deleteHandler(record.id)}
+                        okText="Yes"
+                        cancelText="No"
+                        placement="left"
+                    >
+                        <Button danger icon={<DeleteOutlined />}></Button>
+                    </Popconfirm>
+                </Space>
+            ),
+        },
+    ];
 
 const ProductList: React.FC = () => {
 
@@ -89,8 +90,19 @@ const ProductList: React.FC = () => {
         loadProducts();
     }, []);
 
+    const deleteProduct = async (id: number) => {
+        console.log("Deleting product: ", id);
+
+        const response = await productsService.delete(id);
+        if (response.status === 200) {
+
+            setProducts(products.filter(x => x.id !== id));
+            message.success(`Product deleted successfully!`);
+        }
+    };
+
     return (
-        <Table columns={columns} dataSource={products} />
+        <Table columns={getColumns(deleteProduct)} dataSource={products} />
     );
 }
 
